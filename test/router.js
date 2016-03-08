10b9077035f47674961f926a04b1ace950801828
sky <jyqnba@163.com>
2016-03-08 09:27:27 +0800

    add test node js server

---
+++ test/router.js
@@ -0,0 +1,12 @@
+﻿function route(handle,pathname)
+{
+	console.log("route for "+pathname);
+	if(typeof handle[pathname]==='function'){
+		
+	}
+	else{
+		console.log("route for function "+pathname+" error");
+	}
+}
+
+exports.route=route;
---
+++ test/server.js
@@ -0,0 +1,16 @@
+﻿var http=require("http");
+var url=require("url");
+function start(route,handle){
+ function onrequest(request,response){
+	var pathname=url.parse(request.url).pathname;
+  console.log("reqruese for "+pathname);	
+  route(handle,pathname);
+  response.writeHead(200,{"Content-Type":"text/plain"});
+  response.write("hello world");
+  response.end();
+};
+	http.createServer(onrequest).listen(8888);
+	console.log("start server port:8888");
+}
+
+exports.start=start;
